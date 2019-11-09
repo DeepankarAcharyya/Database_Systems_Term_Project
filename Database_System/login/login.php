56d34ea5c58da9368fe0cc42bf7b90a23df17125
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-11-09 23:37:55 +0530

    sql cmds updated

---
+++ Database_System/login/login.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+    <body>
+        <form method="POST" action="#">
+            <input type="text" name="username" placeholder="Enter Your Username"/>
+            <input type="password" name="password" placeholder="Enter Your Password"/>
+            <input type="submit" name="submit" value="LOGIN" />
+        </form>
+    </body>
+</html>

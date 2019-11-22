f2df895496130744c73eab9a9d7f19e745e1c08f
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-11-22 23:58:01 +0530

    getting started with the menu

---
+++ Database_System/Menu/menu.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>MENU</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        
+        <form action="Player_menu.php">
+        <button type="submit">PLAYER</button>
+        </form>
+        
+        <form action="Umpire_menu.php">
+        <button type="submit">UMPIRE</button>
+        </form>
+        
+        <form action="Stadium_menu.php">
+        <button type="submit">STADIUM</button>
+        </form>
+        
+        <form action="Team_menu.php">
+        <button type="submit">TEAM</button>
+        </form>
+        
+        <form action="Match_menu.php">
+        <button type="submit">MATCH</button>
+        </form>
+        
+    </body>
+</html>
